496531e1fb7863e9b66ff0ef20e3ebcd293c8929
Augustina Ragwitz <auggy@cpan.org>
2016-04-17 19:58:24 -0700

    Extract useful info from inc files for template

--- nova_api_docs_tracker/main.py
+++ nova_api_docs_tracker/main.py
@@ -1,9 +1,75 @@
 # -*- coding: utf-8 -*-
 
+import glob
+import os
+import re
+
+SPLIT_FILE_RE = re.compile(r'\n([\w\s]+)\n\=+\n', re.MULTILINE)
 
 def main():
-    print "foo"
+    # TODO(auggy): args: inc files path
+    path = ''
+    output = {}
 
+    # get inc files
+    inc_files = get_inc_files(path)
+
+    for filename in inc_files:
+        contents = inc_files[filename]
+        # TODO(auggy): link to file on github??
+
+        # TODO(auggy): this should use docutils
+        extracted_contents = extract_stuff(split_inc_file(contents))
+
+        # verify body
+        output['body'] = extracted_contents['body']
+
+        # verify methods names
+        output['methods_list'] = '\n'.join(extracted_contents['methods'].keys())
+
+        methods = {}
+        for method_name in extracted_contents['methods']:
+            method_content = extracted_contents['methods'][method_name]
+
+            # verify parameters
+            # TODO(auggy): create parameters list
+
+            # verify examples
+            # TODO(auggy): I don't think we need anything special here...
+
+            methods[method_name] = method_content
+
+        output['methods'] = methods
+
+        # TODO(auggy): use Jinja templates
+        print "Body: %(body)s \n " + \
+              "Method List: %(methods_list)s \n" + \
+              "Methods: %(methods)s" \
+              % output
+
+
+def get_inc_files(path):
+    inc_files = {}
+    for filename in glob.glob(os.path.join(path, '*.inc')):
+        print "Processing %(filename)s..." % {'filename': filename}
+        f = open(filename, 'r')
+        # TODO(auggy): remove rest of path from filename
+        inc_files[filename] = f.read()
+
+    return inc_files
+
+
+def split_inc_file(contents):
+    split_file = re.split(SPLIT_FILE_RE, contents)
+    return split_file
+
+
+def extract_stuff(split_contents):
+    result = dict()
+
+    result['body'] = split_contents[0]
+    result['methods'] = dict(zip(map(lambda x: re.sub('\\n', '', x), split_contents[3::2]), split_contents[2::2]))
+    return result
 
 # Allow for local debugging
 if __name__ == '__main__':
--- tests/test_nova_api_docs_tracker.py
+++ tests/test_nova_api_docs_tracker.py
@@ -24,6 +24,29 @@ class TestNova_api_docs_tracker(unittest.TestCase):
     def test_000_something(self):
         pass
 
+    def test_check_files(self):
+        path = 'test_files'
+        inc_files = main.get_inc_files(path)
+        expected_names = ["test_files/flavors.inc", "test_files/images.inc", "test_files/servers.inc"]
+        got_names = inc_files.keys()
+        self.assertEquals(expected_names, got_names, "Check that filenames list is correct")
+
+    def test_split_file(self):
+        f = open("test_files/servers.inc", 'r')
+        contents = f.read()
+        got = main.split_inc_file(contents)
+        expected_len = 11
+        self.assertEquals(expected_len, len(got), "Check that file split is correct")
+
+    def test_extract_stuff(self):
+        f = open("test_files/servers.inc", 'r')
+        contents = f.read()
+        split_contents = main.split_inc_file(contents)
+        got = main.extract_stuff(split_contents)
+        expected_keys = ['body', 'methods']
+        expected_methods = sorted(['Delete Server', 'List Servers', 'Show Server Details', 'Update Server'])
+        self.assertEquals(expected_keys, got.keys(), "Check content keys")
+        self.assertEquals(expected_methods, sorted(got['methods'].keys()), "check method keys")
 
 if __name__ == '__main__':
     import sys
