bf218fc8feb3d291fbbfdc180343b54322796012
Augustina Ragwitz <auggy@cpan.org>
2016-04-20 16:48:05 -0700

    WIP: Figure out parameters to create bug in Launchpad

--- nova_api_docs_tracker/main.py
+++ nova_api_docs_tracker/main.py
@@ -7,6 +7,7 @@ import re
 import sys
 
 from nova_api_docs_tracker.templates import templates
+from nova_api_docs_tracker.launchpad import launchpad
 
 SPLIT_FILE_RE = re.compile(r'\n([\w\s]+)\n\=+\n', re.MULTILINE)
 METHODS_LIST_RE = re.compile(r'rest_method:: ([A-Z]+.*$)', re.MULTILINE)
@@ -75,16 +76,22 @@ def main():
         print_local_file(
             contents=lp_out[filename]['body'], output_file='body_' + filename)
 
-        # TODO(auggy): create a bug in LP to verify body content
-        # lp_client.post...?
-        bugs[filename]['body'] = '' # bug id
+        # create a bug in LP to verify body content
+        body_params = get_bug_params(
+            filename=filename, content=lp_out[filename]['body'])
+        # TODO(auggy): try/catch error
+        bug_id = launchpad.create_bug(body_params)
+        bugs[filename]['body'] = bug_id
 
         # print methods locally in case of LP error
         print_local_file(contents=lp_out[filename]['methods_list'], output_file='methods_' + filename)
 
-        # TODO(auggy): create a bug in LP to verify methods list
-        # lp_client.post...?
-        bugs[filename]['methods_list'] = '' # bug id
+        # create a bug in LP to verify method list
+        method_list_params = get_bug_params(
+            filename=filename, content=lp_out[filename]['methods_list'])
+        # TODO(auggy): try/catch error
+        bug_id = launchpad.create_bug(method_list_params)
+        bugs[filename]['methods_list'] = bug_id
 
         for method_name in lp_out[filename]['methods']:
             # print parameters locally in case of LP error
@@ -93,21 +100,30 @@ def main():
                 output_file=
                 '_'.join(method_name.split()) + '_parameters__' + filename)
 
-            # TODO(auggy): create a bug in LP to verify parameters
-            # lp_client.post...?
-            bugs[filename]['methods'][method_name]['parameters'] = '' # bug id
+            # create a bug in LP to verify parameters
+            params_params = get_bug_params(
+                filename=filename,
+                content=lp_out[filename]['methods'][method_name]['parameters'])
+            # TODO(auggy): try/catch error
+            bug_id = launchpad.create_bug(params_params)
+            bugs[filename]['methods'][method_name]['parameters'] = bug_id
 
             print_local_file(
                 contents=lp_out[filename]['methods'][method_name]['examples'],
                 output_file=
                 '_'.join(method_name.split()) + '_examples__' + filename)
 
-            # TODO(auggy): create a bug in LP to verify examples
-            # lp_client.post...?
-            bugs[filename]['methods'][method_name]['examples'] = '' # bug id
+            # create a bug in LP to verify examples
+            examples_params = get_bug_params(
+                filename=filename,
+                content=lp_out[filename]['methods'][method_name]['examples'])
+            # TODO(auggy): try/catch error
+            bug_id = launchpad.create_bug(examples_params)
+            bugs[filename]['methods'][method_name]['examples'] = bug_id
 
     # TODO(auggy): keep track of bugs...?
     # dump out bugs to local file
+
 
 def parse_args():
     parser = argparse.ArgumentParser("Post bugs to Launchpad for verifying API docs")
@@ -143,8 +159,9 @@ def extract_stuff(split_contents):
     result['methods'] = dict(zip(map(lambda x: re.sub('\\n', '', x), split_contents[1::2]), split_contents[2::2]))
     return result
 
+
 def get_methods_list(contents):
-    list = ''
+    methods_list = ''
     for k in contents:
         v = contents[k]
         found = re.search(METHODS_LIST_RE, v)
@@ -152,8 +169,8 @@ def get_methods_list(contents):
             url = found.group(1)
         else:
             continue
-        list = list + url + ' (' + k + ')\n'
-    return list
+        methods_list = methods_list + url + ' (' + k + ')\n'
+    return methods_list
 
 
 def get_parameters_list(contents):
@@ -165,6 +182,25 @@ def print_local_file(contents, output_file):
     print "Writing backup file: {}".format(output_file)
     with open('../out/' + output_file, 'w') as f:
         f.write(contents.encode("utf-8", "ignore"))
+
+
+def get_bug_params(filename, content):
+    params = dict()
+
+    params['title'] = '' # get first line of content
+    params['description'] = '' # content
+    params['information_type'] = 'Public'
+
+    # TODO(auggy): this should be configurable
+    params['target'] = 'nova'
+
+    # space-deliminated keywords
+    params['tags'] = 'api-ref %(filename)s' % {'filename': filename}
+
+    # if test:
+    #   params['private'] = 1
+
+    return params
 
 # Allow for local debugging
 if __name__ == '__main__':
